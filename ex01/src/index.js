44832b34d631b5f0a535900883a30db656f64b92
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-17 01:51:24 +0200

    Added index.js

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,16 @@
+function main(numOne, numTwo){
 
+    var quotient;
+    if(numTwo === 0){
+        quotient = 'Divide by =! Cannot divide by zero!';
+    } else {
+        //only change code below this line
+        quotient = numOne / numTwo;   
+        //only change code above this line
+    }
+
+    return quotient;
+}
+
+console.log(main(15, 3));
+module.exports = main;
