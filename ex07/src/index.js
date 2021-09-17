6dc778cfc77378b08c4a9883810b2e5437c44df4
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-17 02:40:08 +0200

    added index.js

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,1 +1,12 @@
+function main(numOne, numTwo){
+    // only change code below this line
 
+    var numOne = parseInt(numOne), numTwo = parseInt(numTwo);
+    var product = parseInt(numOne % numTwo);
+
+    // only change code above this line
+    return remainder;
+}
+
+console.log(main(5, 2 ));
+module.exports = main;
