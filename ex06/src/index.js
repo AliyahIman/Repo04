81f4002fe4e33b224b47a6d2a121e2acf677e565
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-17 02:30:32 +0200

    added index.js

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,1 +1,12 @@
+function main (numOne, numTwo){
+    // only change code below this line
 
+    var numOne = parseFloat(numOne), numTwo = parseFloat(numTwo);
+    var myQuotient = numOne / numTwo;
+
+    // only change code above this line
+    return myQuotient;
+}
+
+console.log(main(5.0, 2.5));
+module.exports = main;
