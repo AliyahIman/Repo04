e20bfe3bd5c6b09f0ec4f3374c40da1915bb016e
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-17 02:25:31 +0200

    added index.js

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,1 +1,12 @@
+function main (numOne, numTwo){
+    // only change code below this text 
 
+    var numOne, numTwo;
+    var product = numOne * numTwo
+
+    // only change code above this text
+    return product;
+}
+
+console.log(main(2.5, 2.0));
+module.exports = main;
